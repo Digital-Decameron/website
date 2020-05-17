c13ba62f9f62ddb8b81dcd87e04012e80c9c8e8b
Radiergummi <moritz.friedrich@messengerpeople.com>
2020-05-17 17:35:12 +0200

    Fixed progress bar

--- assets/js/progressBar.js
+++ assets/js/progressBar.js
@@ -1,46 +1,43 @@
-window.addEventListener("scroll", updateProgress);
-    
-progressBar = document.getElementById("progressBar");
+window.addEventListener('scroll', updateProgress);
 
-scrollProgress = document.getElementById("progressIndicator");
+progressBar = document.getElementById('progressBar');
 
-if (document.getElementById("subscriptionSection")){
-    articleSubscription = document.getElementById("subscriptionSection").offsetHeight;
-}
-else{
-    articleSubscription = 0;
+scrollProgress = document.getElementById('progressIndicator');
+
+if ( document.getElementById('subscriptionSection') ) {
+  articleSubscription = document.getElementById('subscriptionSection').offsetHeight;
+} else {
+  articleSubscription = 0;
 }
 
-if(document.getElementById("articleNext")){
-    articleNext = document.getElementById("articleNext").offsetHeight;
-}
-else{
-    articleNext = 0;
+if ( document.getElementById('articleNext') ) {
+  articleNext = document.getElementById('articleNext').offsetHeight;
+} else {
+  articleNext = 1000;
 }
 
 footerSection = 150;
 
-bottomOffset = ((articleSubscription + articleNext + footerSection + 250) / document.body.scrollHeight) * 100 ;
+bottomOffset = ( ( articleSubscription + articleNext + footerSection + 250 ) / document.body.scrollHeight ) * 100;
 bottomOffset += bottomOffset * 1.1;
 
+function updateProgress () {
+  let percentScrolled = ( ( window.pageYOffset / document.body.scrollHeight ) * ( 100 + bottomOffset ) );
 
-function updateProgress(){
-    let percentScrolled = ((window.pageYOffset / document.body.scrollHeight) * (100 + bottomOffset));
+  let transformation             = 'translateY(' + percentScrolled + '%)';
+  scrollProgress.style.transform = ( transformation );
 
-    let transformation = "translateY(" + percentScrolled + "%)";
-    scrollProgress.style.webkitTransform = (transformation);
+  console.log(percentScrolled);
 
-
-    if (percentScrolled > 100){
-        progressBar.style.animationName = "progress-fade-out";
-        setTimeout(function(){
-            progressBar.style.opacity = "0";
-        }, 500);
-    }
-    else{
-        progressBar.style.animationName = "progress-fade-in";
-        setTimeout(function(){
-            progressBar.style.opacity = "1";
-        }, 500);
-    }
+  if ( percentScrolled > 100 ) {
+    progressBar.style.animationName = 'progress-fade-out';
+    setTimeout(function () {
+      progressBar.style.opacity = '0';
+    }, 500);
+  } else {
+    progressBar.style.animationName = 'progress-fade-in';
+    setTimeout(function () {
+      progressBar.style.opacity = '1';
+    }, 500);
+  }
 }
